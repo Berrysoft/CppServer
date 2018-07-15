dea8d4bcd63c5ed49dfba8ef616fbc3a48b46731
王宇逸 <Strawberry_Str@hotmail.com>
2018-07-15 09:06:46 +0800

    改到HTTP1.1

--- html_content.cpp
+++ html_content.cpp
@@ -116,7 +116,7 @@ ssize_t html_content::send(int fd, map<string, module> &modules)
         res = deal_commands("error", modules, resp_value, m);
     }
 
-    const char head[] = "HTTP/1.0 %d %s\r\nServer:Berrysoft.Linux.Cpp.Server\r\nContent-Type:text/html;charset=UTF-8\r\n\r\n";
+    const char head[] = "HTTP/1.1 %d %s\r\nServer:Berrysoft.Linux.Cpp.Server\r\nContent-Type:text/html;charset=UTF-8\r\n\r\n";
     char realhead[256];
     memset(realhead, 0, sizeof(realhead));
     switch (resp_value)
--- main.cpp
+++ main.cpp
@@ -24,7 +24,6 @@ int main()
 
     printf("请在自己的电脑上访问 http://127.0.0.1:%d/\n", port);
     printf("网站在Edge、IE、Chrome、Safari(iPhone)下测试通过。\n");
-    printf("请不要过于频繁刷新浏览器。\n");
     printf("按r <回车>刷新模块，q <回车>结束服务器。\n");
 
     char c;
