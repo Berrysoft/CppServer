5ddf3f3b668058c3502de57f96fba0887c16edff
王宇逸 <Strawberry_Str@hotmail.com>
2018-07-20 16:38:43 +0800

    改用C++17

--- Makefile
+++ Makefile
@@ -3,8 +3,8 @@ MODULE := src/mdl/
 BIN := bin/
 OBJ := obj/
 
-STD := -std=c++11 -O3
-STDFIPC := -std=c++11 -fPIC -O3
+STD := -std=c++17 -O3
+STDFIPC := -std=c++17 -fPIC -O3
 
 # 编译所有
 .PHONY: all
--- src/apply_tuple.h
+++ src/apply_tuple.h
@@ -5,28 +5,25 @@
 template <size_t N>
 struct Apply
 {
-    template <typename F, typename T, typename... A>
-    static inline auto apply(F &&f, T &&t, A &&... a)
-        -> decltype(Apply<N - 1>::apply(::std::forward<F>(f), ::std::forward<T>(t), ::std::get<N - 1>(::std::forward<T>(t)), ::std::forward<A>(a)...))
+    template <typename TFunc, typename TTuple, typename... TArg>
+    static inline auto apply(TFunc &&f, TTuple &&t, TArg &&... a)
     {
-        return Apply<N - 1>::apply(::std::forward<F>(f), ::std::forward<T>(t), ::std::get<N - 1>(::std::forward<T>(t)), ::std::forward<A>(a)...);
+        return Apply<N - 1>::apply(std::forward<TFunc>(f), std::forward<TTuple>(t), std::get<N - 1>(std::forward<TTuple>(t)), std::forward<TArg>(a)...);
     }
 };
 
 template <>
 struct Apply<0>
 {
-    template <typename F, typename T, typename... A>
-    static inline auto apply(F &&f, T &&, A &&... a)
-        -> decltype(::std::forward<F>(f)(::std::forward<A>(a)...))
+    template <typename TFunc, typename TTuple, typename... TArg>
+    static inline auto apply(TFunc &&f, TTuple &&, TArg &&... a)
     {
-        return ::std::forward<F>(f)(::std::forward<A>(a)...);
+        return std::forward<TFunc>(f)(std::forward<TArg>(a)...);
     }
 };
 
-template <typename F, typename T>
-inline auto apply(F &&f, T &&t)
-    -> decltype(Apply<::std::tuple_size<typename ::std::decay<T>::type>::value>::apply(::std::forward<F>(f), ::std::forward<T>(t)))
+template <typename TFunc, typename TTuple>
+inline auto apply(TFunc &&f, TTuple &&t)
 {
-    return Apply<::std::tuple_size<typename ::std::decay<T>::type>::value>::apply(::std::forward<F>(f), ::std::forward<T>(t));
+    return Apply<std::tuple_size<typename std::decay<TTuple>::type>::value>::apply(std::forward<TFunc>(f), std::forward<TTuple>(t));
 }
